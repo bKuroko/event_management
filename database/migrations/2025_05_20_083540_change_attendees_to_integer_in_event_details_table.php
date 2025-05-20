c6cefca33e83b18f62209f2d5796e7418163c075
bKuroko <benigay.brent@gmail.com>
2025-05-21 08:39:43 +1400

    Update 2025_05_20_083540_change_attendees_to_integer_in_event_details_table.php

--- database/migrations/2025_05_20_083540_change_attendees_to_integer_in_event_details_table.php
+++ database/migrations/2025_05_20_083540_change_attendees_to_integer_in_event_details_table.php
@@ -11,8 +11,7 @@ return new class extends Migration
      */
     public function up()
 {
-    Schema::table('event_details', function (Blueprint $table) {
-        $table->integer('attendees')->nullable()->change();
+   
     });
 }
 
